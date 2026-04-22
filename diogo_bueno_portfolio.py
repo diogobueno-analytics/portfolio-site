f67eb61c5380d9e7385d8baba933c403df441965
Diogo Bueno <adm.diogobueno@gmail.com>
2026-04-22 11:52:34 -0300

    Update diogo_bueno_portfolio.py

--- diogo_bueno_portfolio.py
+++ diogo_bueno_portfolio.py
@@ -221,7 +221,7 @@ with aba_bio:
     st.markdown("""
     <div style="text-align: left; width: 100%;">
         <h1 class="hero-name" style="margin-bottom: 0px !important;">
-            <span class="highlight">Diogo Bueno da Rosa</span>
+            Diogo Bueno da Rosa
         </h1>
         <div class="hero-subtitle" style="margin-top: 5px !important;">
             // Analista de Business Intelligence | Especialista em Dados
